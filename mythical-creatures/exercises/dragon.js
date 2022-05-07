cfa08ec96b370028e59c3b0f0b2753b3265bfd0b
Irmak Erdem <irmakerdem9@gmail.com>
2022-05-06 18:41:00 -0600

    Add dragon file

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,62 @@
+class Dragon {
+  constructor(nam, rid) {
+    this.name = nam;
+    this.rider = rid;
+    this.hungry = true;
+    this.eatCount = 0;
+  }
+  greet(rid) {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() {
+    this.eatCount++
+    if (this.eatCount >= 3) {
+      this.hungry = false;
+    }
+  }
+}
 
+module.exports = Dragon;
+
+
+//breaks terminal
+// eat() {
+//   for (var i = 0; i = -1; i++) {
+//     // console.log(i);
+//     this.hungry = false;
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Dragon {
+//   constructor(nam, rid) {
+//     this.name = nam;
+//     this.rider = rid;
+//     this.hungry = true;
+//     // this.eatCount = 0;
+//   }
+//   greet(rid) {
+//     return `Hi, ${this.rider}!`
+//   }
+//   eat() {
+//     this.eatCount++
+//     if (this.eatCount >= 3) {
+//       this.hungry = false;
+//     }
+//   }
+// };
